81853044751e4706a3a8e1dcc287b76d13406b39
unknown <ganeww@mail.bg>
2022-09-29 13:05:46 +0300

    Objects and Composition

---
+++ Objects and Composition - Exercise/05. Lowest Prices in Cities.js
@@ -0,0 +1,26 @@
+function lowestPricesInCities(arr) {
+
+    let obj = {}
+
+    for (let index = 0; index < arr.length; index++) {
+        let text = arr[index];
+        let tokens = text.split(' | ')
+        let town = tokens[0]
+        let product = tokens[1]
+        let price = +tokens[2]
+        if (!obj.hasOwnProperty(product)) {
+            obj[product] = { town, price }
+        } else {
+            if (price < obj[product].price) {
+                obj[product] = { town, price }
+            }
+        }
+    }
+    let productNames = Object.keys(obj)
+    for (let index = 0; index < productNames.length; index++) {
+        let productName = productNames[index];
+        console.log(`${productName} -> ${obj[productName].price} (${obj[productName].town})`);
+    }
+}
+
+lowestPricesInCities(['Sample Town | Sample Product | 1000', 'Sample Town | Orange | 2', 'Sample Town | Peach | 1', 'Sofia | Orange | 3', 'Sofia | Peach | 2', 'New York | Sample Product | 1000.1', 'New York | Burger | 10'])
