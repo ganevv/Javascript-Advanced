c284f2fd83aa236b67cdf81cea2968978770584e
unknown <ganeww@mail.bg>
2022-10-14 14:08:27 +0300

    Prototypes and Inheritance

---
+++ Prototypes and Inheritance - Exercise/01. Array Extension .js
@@ -0,0 +1,33 @@
+(function arrayExtension() {
+
+    let arr = [1, 2, 3]
+
+    Array.prototype.last = function () {
+        return this[this.length - 1]
+    }
+
+    Array.prototype.skip = function (n) {
+        let result = []
+
+        for (let index = n; index < arr.length; index++) {
+            result.push(this[index])
+        }
+        return result
+    }
+
+    Array.prototype.take = function (n) {
+        let result = []
+        for (let index = 0; index < n; index++) {
+            result.push(this[index])
+        }
+        return result
+    }
+
+    Array.prototype.sum = function () {
+        return this.reduce((a, b) => a + b, 0)
+    }
+
+    Array.prototype.average = function () {
+        return this.reduce((a, b) => a + b) / arr.length
+    }
+})
